f34f80f7c4c0af31229964a93f017ba2f1adcbf8
Haider <95499061+Haider1234567890@users.noreply.github.com>
2026-01-22 16:50:26 +0000

    changes to enemy script

--- game-jam/Enemy.cs
+++ game-jam/Enemy.cs
@@ -9,5 +9,18 @@ public partial class Enemy : Node2D
 	public override void _Process(double delta)
 	{
 		Position = new Vector2(Position.X, Position.Y + (float)delta * BaseSpeed * SpeedMultiplier);
+		var screenHeight = GetViewportRect().Size.Y;
+		
+		float t = Position.Y / screenHeight;
+		float minScale = 0.6f;
+		float maxScale = 1.8f;
+				
+		Scale = Vector2.One * Mathf.Lerp(minScale, maxScale, t);
+	}
+	
+	public override void _Ready()
+	{
+		var game = GetTree().Root.GetNode<main>("res://main.cs");
+		SpeedMultiplier = game.Difficulty;
 	}
 }
