b2c36dd7d29eb53c82dd6c5259faa8d906554cb4
Haider <95499061+Haider1234567890@users.noreply.github.com>
2026-01-22 15:53:59 +0000

    Started enemy script
    added movement

--- game-jam/Enemy.cs
+++ game-jam/Enemy.cs
@@ -3,13 +3,11 @@ using System;
 
 public partial class Enemy : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	[Export] public float BaseSpeed = 60f;
+	public float SpeedMultiplier = 1f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position = new Vector2(Position.X, Position.Y + (float)delta * BaseSpeed * SpeedMultiplier);
 	}
 }
